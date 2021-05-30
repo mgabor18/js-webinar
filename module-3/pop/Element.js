9999b41d0e838526171c795badb93e6566c4c667
Márton Gábor <gabor_marton1@epam.com>
2021-05-30 17:39:17 +0200

    stuck

--- module-3/pop/Element.js
+++ module-3/pop/Element.js
@@ -1,18 +1,37 @@
 /**
  * Create Element class, which represents an element of
  * the application, and
- * 
+ *
  * 1. It has a protractor locator (.locator),
  *    e.g. by.css("h1.title")
  * 2. It has a name (.name), e.g. "Document Title"
- * 3. It can have a parent Element, 
+ * 3. It can have a parent Element,
  *    which is the context of the element (.parent)
  * 4. It can have children Elements (.children)
  * 5. It has a method to retrieve the protractor element
  *    by the locator (.get([name])) in it's context
  *     - if it gets a name as parameter, it tries to find
  *       in it's children (recursively) the Element with
- *       the given name or throws an Erorr if it cannot
+ *       the given name or throws an Error if it cannot
  *       find the element
  */
-module.exports = class Element {}
+module.exports = class Element {
+  constructor(name, locator) {
+    this.name = name;
+    this.locator = locator;
+    this.parent = null;
+    this.children = {};
+  }
+  setParent(parent) {
+    this.parent = parent;
+  }
+  addChildren(child) {
+    if (this.children.hasOwnProperty(child.name)) {
+      throw new Error(`${child.name} is already added.`);
+    }
+    this.children[child.name] = child;
+  }
+  get() {
+    return;
+  }
+};
--- module-3/pop/Elements.js
+++ module-3/pop/Elements.js
@@ -1,11 +1,11 @@
 /**
  * Create Elements class, which represents a collection of
  * elements of the application, and
- * 
+ *
  * 1. It has a protractor locator (.locator),
  *    e.g. by.css("h1.title")
  * 2. It has a name (.name), e.g. "Document Title"
- * 3. It can have a parent Element, 
+ * 3. It can have a parent Element,
  *    which is the context of the element (.parent)
  * 4. It cannot have children elements
  * 5. It has a method to retrieve all protractor elements
@@ -13,4 +13,16 @@
  * 6. It has a method to retrieve one element from the collection
  *    by the locator (.get(n)) in it's context
  */
-module.exports = class Elements {}
+const Element = require("./Element.js");
+
+module.exports = class Elements extends Element {
+  constructor(name, locator) {
+    super(name, locator);
+
+    this.children = null;
+  }
+  addChildren() {
+    throw new Error("Elements can not have children.");
+  }
+  all() {}
+};
