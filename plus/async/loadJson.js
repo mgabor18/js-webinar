4de28273aa51737faaf2aa9ddce1e314670e7d0e
Márton Gábor <gabor_marton1@epam.com>
2021-06-04 11:05:05 +0200

    adds loadJson.js solution

--- plus/async/loadJson.js
+++ plus/async/loadJson.js
@@ -1,7 +1,7 @@
 /*
- * Rewrite the following code to use async/await 
+ * Rewrite the following code to use async/await
  */
-module.exports = function loadJson(url) {
+/*module.exports = function loadJson(url) {
     return fetch(url)
         .then(response => {
             if (response.status == 200) {
@@ -10,4 +10,15 @@ module.exports = function loadJson(url) {
                 throw new Error(response.status);
             }
         });
+};
+
+*/
+
+module.exports = async function loadJson(url) {
+  const response = await fetch(url);
+  if (response.status === 200) {
+    return response.json();
+  } else {
+    throw new Error(`${response.statusText} ${response.status}`);
+  }
 };
