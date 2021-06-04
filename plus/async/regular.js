a3fc4e8a1c0d4e6d687765562e0638b5b248d4f4
Márton Gábor <gabor_marton1@epam.com>
2021-06-04 11:06:16 +0200

    adds regular.js solution

--- plus/async/regular.js
+++ plus/async/regular.js
@@ -1,13 +1,14 @@
 async function wait() {
-    await new Promise(resolve => setTimeout(resolve, 1000));
+  await new Promise((resolve) => setTimeout(resolve, 1000));
 
-    return 10;
+  return 10;
 }
 
-/* 
+/*
  * You need to call async wait() and wait to get 10 and return it
  * You can't use "await"!
  */
 module.exports = function f() {
-    // ...what to write here?
-}
+  // ...what to write here?
+  return wait();
+};
