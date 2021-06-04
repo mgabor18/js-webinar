a0b06602d8227f841b1e180d88871a4dcea0c678
Márton Gábor <gabor_marton1@epam.com>
2021-06-04 14:01:21 +0200

    finalizing sleep.js

--- plus/async/sleep.js
+++ plus/async/sleep.js
@@ -9,7 +9,7 @@
 module.exports = async function sleep(sec) {
   return new Promise((resolve, reject) => {
     if (isNaN(sec)) {
-      reject(new Error("Sec parameter must be a number"));
+      return reject(new Error("Sec parameter must be a number"));
     }
     setTimeout(resolve, Math.min(sec * 1000, 10000));
   });
