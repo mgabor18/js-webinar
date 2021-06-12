3b0b36d99a6821cc9489261ae5f5f60156bd7553
Márton Gábor <gabor_marton1@epam.com>
2021-06-12 18:20:52 +0200

    adds arraySum solution

--- module-4/arraySum.js
+++ module-4/arraySum.js
@@ -6,3 +6,18 @@
  * @param {Array} elements
  * @returns {number} summary of all integers or 0 in other cases
  */
+
+module.exports = function arraySum(elements) {
+  let result = 0;
+  if (elements.length === 0 || !Array.isArray(elements)) {
+    return result;
+  }
+  for (let el of elements) {
+    if (typeof el === "number") {
+      result += el;
+    } else if (Array.isArray(el)) {
+      result += arraySum(el);
+    }
+  }
+  return result;
+};
