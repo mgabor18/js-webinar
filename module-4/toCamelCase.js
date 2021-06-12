af3b4981952852c960f4ac885f84651e6df1baf5
Márton Gábor <gabor_marton1@epam.com>
2021-06-12 18:21:53 +0200

    adds toCamelCase solution

--- module-4/toCamelCase.js
+++ module-4/toCamelCase.js
@@ -5,3 +5,30 @@
  * @param {string} toConvert
  * @returns {string} camel-case string or empty string in other cases
  */
+const ALLOWED_CHARACTERS = "abcdefghijklmnopqrstuvwxyz0123456789";
+module.exports = function toCamelCase(str) {
+  if (typeof str !== "string") {
+    return "";
+  }
+  return str
+    .replace(/\s/g, " ")
+    .split(" ")
+    .map((word) => {
+      return word
+        .split("")
+        .filter((c) => {
+          return ALLOWED_CHARACTERS.includes(c.toLowerCase());
+        })
+        .join("");
+    })
+    .filter((word) => {
+      return word !== "";
+    })
+    .map((word, i) => {
+      if (i === 0) {
+        return word.toLowerCase();
+      }
+      return word[0].toUpperCase() + word.slice(1).toLowerCase();
+    })
+    .join("");
+};
